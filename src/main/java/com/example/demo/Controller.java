c86e2db904f29dc6aa521e3e4b38fe49a037e05b
xiexed <xiexed@gmail.com>
2018-07-08 23:31:42 +0300

    GetMapping

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
@@ -15,7 +16,7 @@ public class Controller {
     @Autowired
     ReactiveCommentsReository commentsReository;
 
-    @PutMapping("/firstComments")
+    @GetMapping("/firstComments")
     public Flux<String> allComments() {
         return blogpostReository.findAll().flatMap(post -> {
             String first = commentsReository.findComments(post.getId()).blockFirst();
